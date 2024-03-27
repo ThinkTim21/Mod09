426e9f1e218bf01877ef3effd540448d0440e681
Timothy Rodriguez <timrodriguez21@gmail.com>
2024-03-26 20:46:25 -0400

    get setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/ThinkTim21/Mod09/tree/main',
+    author='T. A. Rodriguez',
+    author_email='tar3kh@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
